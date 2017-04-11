6fe9c5d354e796dfe104ec8c1baab4fe068aa31c
Mateisz Brycki <mateusz.brycki@outlook.com>
2017-04-11 22:53:19 +0200

    F-8 - encapsulating aksesi-gesture.js logic into one class,r efactoring

--- front/public/js/aksesi-gestures.js
+++ front/public/js/aksesi-gestures.js
@@ -1,43 +1,90 @@
-//TODO mbrycki close all those loginc in a class
-var isMouseButtonPressed = false;
-var gestureNumber = 0;
+function AksesiEventHandler() {
+    this.isMouseButtonPressed = false;
+    this.gestureNumber = 0;
 
-var pointsArray = [];
-var pointsArrayNumber = 0;
+    this.pointsArray = new Array();
+    this.pointsArrayNumber = 0;
 
-var passwordElementsHandler = new PasswordElementsHandler();
+    this.passwordElementsHandler = new PasswordElementsHandler();
 
-function processGestureStoring() {
-    var element = new Gesture(pointsArray);
-    passwordElementsHandler.addElement(element);
-    resetPointArray();
-}
+    this.resetModuleState= function(form) {
+        this.isMouseButtonPressed = false;
+        this.gestureNumber = 0;
 
-function resetPointArray() {
-    pointsArray = [];
-    pointsArrayNumber = 0;
-}
+        this.pointsArray = [];
+        this.pointsArrayNumber = 0;
 
-function appendGestureCharacterRepresentation() {
-    var characterAreaInput = $(CHARACTER_AREA_NAME_DOT).val();
-    $(CHARACTER_AREA_NAME_DOT).val(characterAreaInput + GESTURE_CHARACTER_REPRESENTATION);
-}
+        this.passwordElementsHandler = new PasswordElementsHandler();
 
-function resetModuleState(form) {
-    isMouseButtonPressed = false;
-    gestureNumber = 0;
+        if (form !== null && form.length > 0) {
+            form[0].reset();
+        }
+    };
 
-    pointsArray = [];
-    pointsArrayNumber = 0;
+    this.mouseDownEvent = function(e) {
+        this.isMouseButtonPressed = true;
+        this.gestureNumber++;
 
-    passwordElementsHandler = new PasswordElementsHandler();
+        this._logDeveloperInfo('Mouse down event');
+    };
 
-    if(form !== null && form.length > 0) {
-        form[0].reset();
-    }
-}
+    this.mouseMoveEvent = function(e) {
+        if (this.isMouseButtonPressed) {
+            this.pointsArray[this.pointsArrayNumber] = new Point(e.pageX, e.pageY);
+            this.pointsArrayNumber++;
+
+            this._logDeveloperInfo('Mouse move event: (' + e.pageX + ', ' + e.pageY + ')');
+        }
+    };
+
+    this.mouseUpEvent = function(e) {
+        this.isMouseButtonPressed = false;
+        this._processGestureStoring();
+
+        //change focus to the character input
+        $(CHARACTER_AREA_NAME_DOT).focus();
+        this._appendGestureCharacterRepresentation();
+
+        this._logDeveloperInfo('Mouse up event');
+    };
+
+    this.keyPressEvent = function(e) {
+        var character = String.fromCharCode(e.which);
+        var key = new Character(character);
+        this.passwordElementsHandler.addElement(key);
+    };
+
+    this.keyUpEvent = function(e) {
+        //handling backspace
+        if (e.which == 8) {
+            this.passwordElementsHandler.removeLastPressedKey();
+        }
+    };
+
+    this.getPassword = function() {
+        return this.passwordElementsHandler.getPassword();
+    };
+
+    this._logDeveloperInfo = function(message) {
+        $(DEVELOPER_INFO_AREA_NAME).append(message + ', number: ' + this.gestureNumber + '<br />');
+    };
 
 
-function logDeveloperInfo(message) {
-    $(DEVELOPER_INFO_AREA_NAME).append(message + ', number: ' + gestureNumber + '<br />');
+    this._appendGestureCharacterRepresentation = function() {
+        var characterAreaInput = $(CHARACTER_AREA_NAME_DOT).val();
+        $(CHARACTER_AREA_NAME_DOT).val(characterAreaInput + GESTURE_CHARACTER_REPRESENTATION);
+    };
+
+
+    this._processGestureStoring = function() {
+        var element = new Gesture(this.pointsArray);
+        this.passwordElementsHandler.addElement(element);
+        this._resetPointArray();
+    };
+
+    this._resetPointArray = function() {
+        this.pointsArray = [];
+        this.pointsArrayNumber = 0;
+    };
+
 }
